8b6d6b58c03e22ffe30c407710b9d80e05b54eb5
Islam Osama <144542260+islamosamaahmed@users.noreply.github.com>
2025-09-26 02:39:28 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'Lucky Tech Hub Bot',
+  author: '‎',
+  botName: "Lucky Tech Hub Bot",
+  botOwner: 'Lucky 218', // Your name
+  ownerNumber: '256789101112', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.2.5",
+  updateZipUrl: "https://www.mediafire.com/file/zlbozqwb014g68w/LuckyTechHub-Bot-main.zip/file",
+};
+
+module.exports = settings;
